4535850e33f68e344939bb4fc1d7ff39129f2b3a
Arshvir <arshvirc@gmail.com>
2023-09-04 15:57:45 -0400

    Create temporary home section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,31 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 
+import picture from "../assets/home.jpg"
+import car from "../assets/redbull.avif"
+
 const Hero = () => {
   return (
-    <section className='lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'>
-        <div className="container mx-auto">hero </div>
+    <section id='home' className='lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'>
+        <div className="container mx-auto">
+          <div className="flex items-center h-full pt-8">
+            {/* left side */}
+            <div className="flex flex-1 flex-col items-center lg:items-start">
+              <p className='text-lg text-accent mb-[22px]'>Hi! My Name Is Arshvir,</p>
+              <h1 className='text-4xl leading-[44px] md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>I am a Software Developer</h1>
+              <p className='pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:text-left'>
+                With an interest in machine learning and artifical intelligenc
+              </p>
+              <button className='btn btn-md bg-accent hover:bg-accent-hover md:btn-lg transition-all'>
+                Learn more about me
+              </button>
+            </div>
+            {/* right side */}
+            <div className="hidden lg:flex flex-1 justify-end items-end">
+              <img src={car} alt='' />
+            </div>
+          </div>
+        </div>
     </section>
   )
 }
