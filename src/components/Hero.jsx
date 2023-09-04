14c4e0cf41d55303cf22f68d4f1d5d9075bc88ba
Arshvir <arshvirc@gmail.com>
2023-09-04 12:52:39 -0400

    Commit Header + Navigation

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'>
+        <div className="container mx-auto">hero </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Socials.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Socials = () => {
+  return (
+    <div>Socials</div>
+  )
+}
+
+export default Socials
